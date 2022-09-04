e30758d7b69cb57c5639d2dc12289bb2dc7d9f67
abdumajidRashidov <abdumajidrashidov44@gmail.com>
2022-09-04 22:54:45 +0500

    first commit

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@)0h$p%e=m)hw=r(cig__9y%-%$(^=k#3g^jmvb@r$*w+&k@8b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
